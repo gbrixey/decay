322d2c9e15695cc6863e3c6b83224ff1e39cbf60
Glen Brixey <glen.brixey@gmail.com>
2018-11-17 16:49:15 -0500

    Add app code

---
+++ app.py
@@ -0,0 +1,134 @@
+from flask import Flask, flash, g, redirect, render_template, request
+from degrade import degrade_text, degrade_jpeg
+from uuid import uuid4
+import os
+import sqlite3
+from werkzeug.utils import secure_filename
+
+DATABASE = 'data.db'
+MAX_FILES = 20
+UPLOAD_FOLDER = 'static/images'
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def ensure_upload_directory():
+    """Creates the image upload directory if necessary."""
+    os.makedirs(UPLOAD_FOLDER, exist_ok = True)
+
+def ensure_items_table():
+    """Creates the database table if necessary."""
+    db = sqlite3.connect(DATABASE)
+    sql_create_table = '''
+        create table if not exists items (
+            id integer primary key,
+            filename text,
+            description text
+        );
+    '''
+    db.cursor().execute(sql_create_table)
+    db.close()
+
+def is_valid_jpeg_file(image_file):
+    """Returns True if I think the given file is a jpeg, or False otherwise.
+    This function just performs some basic checks to weed out people uploading
+    the wrong file type."""
+    # Check file extension first.
+    filename = image_file.filename
+    jpeg_extensions = set(['jpg', 'jpeg'])
+    if '.' in filename and filename.rsplit('.', 1)[1].lower() in jpeg_extensions:
+        # Then check the actual image data.
+        b = bytearray(image_file.read())
+        image_file.seek(0)
+        return b[0] == 0xFF and b[1] == 0xD8 and b[-2] == 0xFF and b[-1] == 0xD9
+    return False
+
+def degrade_database():
+    """Degrades all images and text currently in the database."""
+    sql_fetch = '''select * from items'''
+    items = g.db.cursor().execute(sql_fetch).fetchall()
+    for item in items:
+        id = item['id']
+        description = item['description']
+        old_filename = item['filename']
+        old_image_path = UPLOAD_FOLDER + '/' + old_filename
+        new_filename = old_filename[:-13] + '_' + small_uuid() + '.jpg'
+        new_image_path = UPLOAD_FOLDER + '/' + new_filename
+        degrade_jpeg(old_image_path, new_image_path)
+        os.remove(old_image_path)
+        new_description = degrade_text(description)
+        sql_update = '''
+            update items
+            set filename = ?,
+                description = ?
+            where id = ?
+        '''
+        g.db.cursor().execute(sql_update, (new_filename, new_description, id))
+    g.db.commit()
+    return
+
+def trim_database_if_necessary():
+    """This function ensures the database does not contain more than
+    the maximum number of images."""
+    maximum_number_of_images = 10
+    sql_fetch = '''select id from items order by id desc'''
+    items = g.db.cursor().execute(sql_fetch).fetchall()
+    for (index, item) in enumerate(items):
+        if index >= maximum_number_of_images:
+            id = items['id']
+            sql_delete = '''delete from items where id = ?'''
+            g.db.cursor().execute(sql_delete, (id,))
+    g.db.commit()
+    return
+
+def small_uuid():
+    """Returns the first 8 characters of a random uuid."""
+    return str(uuid4())[:8]
+    
+def makedict(cursor, row):
+    return dict((cursor.description[i][0], value) for i, value in enumerate(row))
+    
+@app.before_request
+def before_request():
+    g.db = sqlite3.connect(DATABASE)
+    g.db.row_factory = makedict
+
+@app.after_request
+def after_request(response):
+    g.db.close()
+    return response
+    
+@app.route('/')
+def main_page():
+    # Get image names and descriptions from database
+    sql_fetch = '''select * from items order by id desc'''
+    items = g.db.cursor().execute(sql_fetch).fetchall()
+    return render_template('index.html', items = items)
+
+# TODO: Maybe crop new image to the desired height? (Croppie)
+# TODO: Find a way to add font data to the description
+@app.route('/submit_image', methods=['POST'])
+def submit_image():
+    image_file = request.files.get('image')
+    if image_file == None or not is_valid_jpeg_file(image_file):
+        return redirect('/')
+    components = image_file.filename.rsplit('.', 1)
+    filename = secure_filename(components[0] + '_' + small_uuid() + '.jpg')
+    image_file.save(os.path.join(UPLOAD_FOLDER, filename))
+    # Degrade existing images and text before inserting the new image,
+    # so that the new image will start off with no glitches.
+    degrade_database()
+    description = request.form['description']
+    if len(description) > 1000:
+        description = description[:1000]
+    sql_insert = '''insert into items (filename, description) values (?, ?)'''
+    g.db.cursor().execute(sql_insert, (filename, description))
+    g.db.commit()
+    # Remove the oldest image if necessary.
+    trim_database_if_necessary()
+    return redirect('/')
+    
+if __name__ == '__main__':
+    ensure_upload_directory()
+    ensure_items_table()
+    app.run(debug = True)
---
+++ degrade.py
@@ -0,0 +1,78 @@
+import math
+import random
+
+def jpeg_header_length(byte_array):
+    """Finds the length of a jpeg header, given the jpeg data in byte array format"""
+    result = 417
+    for i in range(len(byte_array) - 3):
+        if byte_array[i] == 0xFF and byte_array[i + 1] == 0xDA:
+            result = i + 2
+            break
+    return result
+
+last_seed_used = 0
+    
+def degrade_jpeg_byte_array(byte_array):
+    """Sets some of the bytes in the given jpeg byte array to zero.
+    Number of bytes affected depends on the size of the array."""
+    # Try not to edit the jpeg header.
+    header_length = jpeg_header_length(byte_array)
+    max_index = len(byte_array) - header_length - 4
+    iterations = int(len(byte_array) / 50000)
+    seed = random.randint(0, 100)
+    while seed == last_seed_used:
+        seed = random.randint(0, 100)
+    seed_percent = seed / 100
+    for i in range(iterations):
+        min_pixel_index = math.floor(max_index / iterations * i)
+        max_pixel_index = math.floor(max_index / iterations * (i + 1))
+        delta = max_pixel_index - min_pixel_index
+        pixel_index = math.floor(min_pixel_index + delta * seed_percent)
+        if pixel_index > max_index:
+            pixel_index = max_index
+        index_in_byte_array = math.floor(header_length + pixel_index)
+        byte_array[index_in_byte_array] = 0x00
+    return byte_array
+
+def degrade_jpeg(input_file, output_file):
+    """Randomly edits bytes in the given jpeg file and writes the result to the output file."""
+    with open(input_file, 'rb') as f:
+        input_byte_array = bytearray(f.read())
+    output_byte_array = degrade_jpeg_byte_array(input_byte_array)
+    with open(output_file, 'wb') as f:
+        f.write(output_byte_array)
+        
+def degrade_text(text):
+    """Adds several typographic errors to the given string and returns the result.
+    The number of errors added depends on the length of the string."""
+    count = int(len(text) / 8)
+    # Add the delete function twice so that the length of the string will stay roughly the same.
+    options = [delete_character, delete_character, swap_two_characters, duplicate_character, insert_character_from_text]
+    for _ in range(count):
+        option = random.choice(options)
+        text = option(text)
+    return text
+    
+def swap_two_characters(text):
+    """Swaps two adjacent characters in the given string and returns the result."""
+    index = random.randint(0, len(text) - 2)
+    return text[:index] + text[index + 1] + text[index] + text[(index + 2):]
+    
+def duplicate_character(text):
+    """Doubles up a character in the given string and returns the result.
+    e.g. "sample" -> "samplle"
+    """
+    index = random.randint(0, len(text) - 1)
+    return text[:index] + text[index] + text[index] + text[(index + 1):]
+    
+def insert_character_from_text(text):
+    """Selects a character in the given string, inserts a copy of that character
+    somewhere else in the string, and returns the result."""
+    new_character = random.choice(text)
+    index = random.randint(0, len(text))
+    return text[:index] + new_character + text[index:]
+
+def delete_character(text):
+    """Deletes a character from the given string and returns the result."""
+    index = random.randint(0, len(text) - 1)
+    return text[:index] + text[(index + 1):]
